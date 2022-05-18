43a59d41360e581f3cf131f6252c1dee3f8193d0
divadiahim <marian1974@gmail.com>
2022-05-18 17:10:29 +0300

    fonts,lives and other logic

--- game.cpp
+++ game.cpp
@@ -4,8 +4,7 @@
 #include <random>
 #include <cstdlib>
 #include <iostream>
-#define RAYBLACK \
-    CLITERAL(Color) { 0, 0, 0, 0 }
+#define RAYBLACK CLITERAL(Color) { 0, 0, 0, 0 }
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 1000
 int a[10][10];
@@ -37,23 +36,6 @@ float calculate_angle(Vector2 navaPosition)
     DrawText(TextFormat("%f", angle * (180.0 / 3.141592653589793238463)), 100, 10, 20, RED);
     return angle * (180.0 / 3.141592653589793238463);
 }
-typedef struct Shoot
-{
-    Vector2 start_poz;
-    Vector2 end_poz;
-    Vector2 speed;
-    bool active;
-    Color color;
-} Shoot;
-
-// max bullets
-#define NUM_SHOOTS 50
-
-// array of bullets to reuse
-static Shoot shoot[NUM_SHOOTS];
-
-// to reduce the shoot per second
-static int shootRate;
 struct bullet
 {
     float len = 10;
@@ -63,23 +45,18 @@ struct bullet bullets[100];
 float o1 = 0.005;
 float l1 = 15;
 Rectangle rect1;
-
+bool check_collided(Vector2 poz1, int rad1, Vector2 poz2,int rad2)
+{
+        bool planet_collision = CheckCollisionCircles(poz1,  rad1, poz2, rad2);
+        return planet_collision;
+}
+Vector2 bam;
 void bullet(Vector2 navaPosition,Vector2 mousePosition)
 {
     static int offset = 1;
-
-    // get mouse coordinates
-    //Vector2 mousePosition = GetMousePosition();
-    // int a = mousePosition.y - navaPosition.y;
-    // int b = mousePosition.x - navaPosition.x;
-    //  float angle = (float)atan2(mousePosition.y - navaPosition.y, mousePosition.x - navaPosition.x);
-    // DrawText(TextFormat("%f %f",len*sin(angle),len*sin(angle)), 200, 100, 20, RED);
-    // int len2=sqrt(a*a+b*b);
-    // DrawLineEx(Vector2{(float)(navaPosition.x),(float)(navaPosition.y)},Vector2{(float)((len+len2)*cos(angle)),(float)((len+len2)*sin(angle))},10,RED);
+    bool planet_collision = false;
     Vector2 start_pos = {navaPosition.x, navaPosition.y};
     Vector2 end_pos = {mousePosition.x, mousePosition.y};
-    // Vector2 start_pos = {500,500};
-    // Vector2 end_pos = {100,20};
     Vector2 poz_3 = Vector2Normalize(Vector2Subtract(end_pos, start_pos));
 
     // print len and offset
@@ -87,16 +64,15 @@ void bullet(Vector2 navaPosition,Vector2 mousePosition)
     for (int i = 0; i < 1; i++)
     {
         bullets[i].len += l1;
-        std::cout << bullets[i].len << " " << bullets[i].offset << std::endl;
-        // start_pos.x+=bullets[i].offset;
-        // start_pos.y+=bullets[i].offset;
+     //   std::cout << bullets[i].len << " " << bullets[i].offset << std::endl;
         poz_3.x *= bullets[i].len;
         poz_3.y *= bullets[i].len;
         Vector2Normalize(start_pos);
-        // start_pos.x*=bullets[i].offset;
-        // start_pos.y*=bullets[i].offset;
-        //   DrawLineEx(start_pos,Vector2Add(start_pos,poz_3),10,RED);
+      
         DrawCircleV(Vector2Add(start_pos, poz_3), 10, RED);
+        bam=Vector2Add(start_pos, poz_3);
+
+        //check_collided(Vector2{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2},170,Vector2Add(start_pos, poz_3),10);
     }
 }
 bool prssed = 0;
@@ -104,15 +80,20 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-
     InitAudioDevice();
     Sound fxWav = LoadSound("../assets/sf_laser_15.mp3");
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] game - gravitas");
     Texture2D planet = LoadTexture("../assets/planets.png");
     Texture2D spaceship = LoadTexture("../assets/spaceship.png");
-    // draw a black background
+    Font font = LoadFont("../assets/pcsenior.ttf");
     int_bg();
     SetTargetFPS(144);
+    //--------------------------------------------------------------------------------------
+    // draw a black background
+    //variable-initialization
+    //--------------------------------------------------------------------------------------
+    int lives_no = 3;
+    int lives_planet = 10;
     float framewidth = planet.width / 50;
     float frameheight = planet.height / 4;
     float framewidth_space = spaceship.width / 18;
@@ -125,29 +106,16 @@ int main(void)
     Vector2 mousepoz;
     int max_frames_w = planet.width / framewidth;
     int max_frames_h = planet.height / frameheight;
+    bool planet_collision = false;
+    bool last_state = false;
+    bool current_state = false;
     Vector2 navaPosition = {(float)0, (float)0};
-    // Initialize shoots
     //--------------------------------------------------------------------------------------
-    // for (int i = 0; i < NUM_SHOOTS; i++)
-    // {
-    //     shoot[i].start_poz.x = navaPosition.x;
-    //     shoot[i].start_poz.y = navaPosition.y;
-    //     shoot[i].end_poz.x = 5;
-    //     shoot[i].rect.height = 10;
-    //     shoot[i].speed.x = 0;
-    //     shoot[i].speed.y = -10;
-    //     shoot[i].active = false;
-    //     shoot[i].color = MAROON;
-    // }
-    // //--------------------------------------------------------------------------------------
-    // a line strip
-
     while (!WindowShouldClose())
     {
         BeginDrawing();
         DrawText(TextFormat("Mouse: %.1f, %.1f", GetMousePosition().x, GetMousePosition().y), 400, 10, 20, RED);
         DrawText(TextFormat("Screen Size: %i, %i", SCREEN_WIDTH, SCREEN_HEIGHT), 400, 30, 20, RED);
-
         if (IsKeyDown(KEY_D))
             navaPosition.x += 2.0f;
         if (IsKeyDown(KEY_A))
@@ -182,7 +150,7 @@ int main(void)
         DrawFPS(10, 10);
         //print prssed variable
     
-        DrawText(TextFormat("%d prssed",prssed), 700, 10, 20, RED);
+       
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && prssed == 0)
         {
             prssed = 1;
@@ -209,13 +177,25 @@ int main(void)
             timer2 = 0.0f;
             bullets[0].len = 0;
         }
-   
+        
+        //help me overcome my depression+ge
+        std::cout<<lives_planet<<std::endl;
+        DrawTextEx(font, TextFormat("Lives %d",lives_no), Vector2 {750,10}, 30, 1, Color {255,255,255,255}); 
+        DrawRectangleRoundedLines(Rectangle{750,100,230,50}, 0.7, 4,4, Color {70,52,235,255});
+        DrawRectangleRounded(Rectangle{750,100,(230/10)*(float)lives_planet,50}, 0.7, 4, Color {70,52,235,255});
+        planet_collision=check_collided(Vector2{SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2},170,bam,10);
+        current_state  = planet_collision;
+        //random value between 0 and 1
+        float rand1= (planet_collision)?(((float)rand() / (float)RAND_MAX)/1)*(1-0.85)+0.85 :1;
+        float rand2= (planet_collision)?(((float)rand() / (float)RAND_MAX)/1)*(1-0.85)+0.85 :1;
+        lives_planet = (current_state && !last_state)?lives_planet-1:lives_planet,last_state=current_state;
         float degrees = calculate_angle(navaPosition);
-        DrawTexturePro(planet, Rectangle{framewidth * frame, frameheight * frame2, framewidth, frameheight}, Rectangle{SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, framewidth * 3, frameheight * 3}, Vector2{0.5, 0.5}, 0, WHITE);
+        DrawTexturePro(planet, Rectangle{framewidth * frame, frameheight * frame2, framewidth, frameheight}, Rectangle{(SCREEN_WIDTH /3)*(rand1) , (SCREEN_HEIGHT / 3)*(rand2) , framewidth * 3, frameheight * 3}, Vector2{0.5, 0.5}, 0, WHITE);
         DrawTextureTiled(spaceship, Rectangle{framewidth_space * 5, 0, framewidth_space, frameheight_space}, Rectangle{navaPosition.x, navaPosition.y, framewidth_space, frameheight_space}, Vector2{0, 0}, degrees - 90, 1, WHITE);
-        // DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        // draw the spaceship at the angle calculated above
-
+            // if (planet_collision)
+            // {
+            //     DrawText("You died!", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20, RED);
+            // }
         EndDrawing();
     }
 
